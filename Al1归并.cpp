c483a18e4a7d16baeb00bfffb5cc177f6775234c
ranle <ranle336699@gmail.com>
2025-04-08 09:52:18 +0800

    归并代码完善

--- Al1归并.cpp
+++ Al1归并.cpp
@@ -2,6 +2,35 @@
 #include<vector>
 using namespace std;
 
-vector<int> merge() {
+void merge(vector<int> arr,int L,int Mid,int R) {
+
+	int help = 0;
+	int p1 = L;
+	int p2 = Mid;
+
+	while (p1 <= Mid && p2 <= R) {
+		arr[help++ + L] = arr[p1] < arr[p2] ? arr[p1++] : arr[p2++];
+	}
+
+	while (p1 <= Mid) {
+		arr[help++ + L] = arr[p1++];
+	}
+
+	while (p2 <= R) {
+		arr[help++ + L] = arr[p2++];
+	}
+
+}
+
+void guiBing(vector<int> arr,int L,int R) {
+
+	int Mid = L + ((R - L) >> 1);
+
+	//◊Û≈≈–Ú
+	guiBing(arr, L, Mid);
+	//”“≈≈–Ú
+	guiBing(arr, Mid + 1, R);
+	//∫œ≤¢
+	merge(arr, L, Mid, R);
 
 }
