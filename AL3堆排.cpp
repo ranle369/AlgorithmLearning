4b0e7190005ba16d85492cc1423878450f21b829
ranle <ranle336699@gmail.com>
2025-04-11 09:39:47 +0800

    函数定义要放到前面去

--- AL3堆排.cpp
+++ AL3堆排.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
 using namespace std;
 
+void heapify(int* arr, int heapSize, int index) {
+
+	int leftChild = index * 2 + 1;
+	int maxIndex;
+
+	//使用下标判断，是否有还需要继续往深层比较大小
+	while (leftChild < heapSize) {
+
+		//两个孩子比较，谁大放入谁的下标
+		maxIndex = arr[leftChild] > arr[leftChild + 1] && (leftChild + 1) < heapSize ? leftChild : leftChild + 1;
+
+		//父亲和大孩子比较
+		maxIndex = arr[index] > arr[maxIndex] ? index : maxIndex;
+
+		if (maxIndex == index) break;
+
+		swap(arr[index], arr[maxIndex]);
+
+		index = maxIndex;
+
+		leftChild = index * 2 + 1;
+
+	}
+
+}
+
 void heapSort(int *arr) {
 
 	int heapSize = sizeof(arr) / sizeof(arr[0]);
@@ -39,28 +65,3 @@ void heapInser(int* arr, int index) {
 	}
 }
 
-void heapify(int *arr, int heapSize, int index) {
-
-	int leftChild = index * 2 + 1;
-	int maxIndex;
-
-	//使用下标判断，是否有还需要继续往深层比较大小
-	while (leftChild < heapSize) {
-
-		//两个孩子比较，谁大放入谁的下标
-		maxIndex = arr[leftChild] > arr[leftChild + 1] && (leftChild + 1) <  heapSize? leftChild : leftChild + 1;
-
-		//父亲和大孩子比较
-		maxIndex = arr[index] > arr[maxIndex] ? index : maxIndex;
-
-		if (maxIndex == index) break;
-
-		swap(arr[index], arr[maxIndex]);
-
-		index = maxIndex;
-
-		leftChild = index * 2 + 1;
-
-	}
-
-}
