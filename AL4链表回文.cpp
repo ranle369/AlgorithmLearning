0d0978d2ebea65b956fc4fac524ab038c10a67ba
ranle <ranle336699@gmail.com>
2025-04-13 22:40:48 +0800

    空间复杂度为1，使用的时对于链表的逆序思想，逆序时需要使用三个指针，head，temp，prehead

--- AL4链表回文.cpp
+++ AL4链表回文.cpp
@@ -8,7 +8,7 @@ struct Node {
 };
 
 //使用n/2的内存栈空间存储链表然后，进行比照
-bool stackHuiWen(Node *head) {
+bool stackHuiWen1(Node *head) {
 	if (head->next == NULL || head == NULL) {
 		return true;
 	}
@@ -36,4 +36,62 @@ bool stackHuiWen(Node *head) {
 	}
 
 	return true;
+}
+
+//对于n/2空间复杂度优化，空间复杂度为1
+bool huiWen(Node *head) {
+	if (head == NULL || head->next == NULL) {
+		return true;
+	}
+
+	Node *n1 = head;
+	Node* n2 = head;
+
+	while (n1->next != NULL && n2->next->next != NULL) {
+		n1 = n1->next;
+		n2 = n2->next->next;
+	}
+
+	//使用头插法思想，把n1后面的node进行逆序连接到回文中点
+	/*
+	while (n1->next != NULL) {
+		temp = n1;
+		n1 = n1->next;
+		temp->next = PHead;
+		PHead = temp;
+	}*/
+
+	Node* NewHead = n1->next;
+	n1->next = NULL;
+	Node* PreNode = n1;
+	Node* Temp = NULL;
+	//优化头插法
+	while (NewHead != NULL) {
+		Temp = NewHead;
+		NewHead = NewHead->next;
+		Temp->next = PreNode;
+		PreNode = Temp;
+	}
+	
+	n1 = head;
+	n2 = NewHead;
+
+	while (n1 != NULL && n2 != NULL) {
+		if (n1->data != n2->data) {
+			return false;
+		}
+		n1 = n1->next;
+		n2 = n2->next;
+	}
+
+	PreNode = NULL;
+	while (NewHead != NULL) {
+		Temp = NewHead;
+		NewHead = NewHead->next;
+		Temp->next = PreNode;
+		PreNode = Temp;
+	}
+
+	return true;
+
 }
